f4f3607306a0421b556e7e6dc80476fa80588390
Natalie Polk <npolk@g.clemson.edu>
2016-05-31 16:01:39 +0200

    Navigation between pages works, registration page in the works

---
+++ OnboardingDemo/LandingViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LandingViewController.swift
+//  OnboardingDemo
+//
+//  Created by Serene Mirza on 5/31/16.
+//  Copyright © 2016 Natalie Polk. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Whatever title"
+
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButtonTapped(sender: UIButton) {
+        
+        print("login button tapped")
+        let lvc = LoginViewController(nibName: "LoginViewController", bundle: nil)
+        self.navigationController?.pushViewController(lvc, animated: true)
+    }
+    
+   
+    @IBAction func registerButtonTapped(sender: AnyObject) {
+        print("register button tapped")
+        let rvc = RegistrationViewController(nibName: "RegistrationViewController", bundle: nil)
+        self.navigationController?.pushViewController(rvc, animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ OnboardingDemo/RegistrationViewController.swift
@@ -0,0 +1,44 @@
+//
+//  RegistrationViewController.swift
+//  OnboardingDemo
+//
+//  Created by Serene Mirza on 5/31/16.
+//  Copyright © 2016 Natalie Polk. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Register"
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func registerButtonPressed(sender: UIButton) {
+        var (failmessage, user) = UserController.sharedInstance.registerUser(emailField.text!, newPassword: passwordField.text!)
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
